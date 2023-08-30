aa497e9ce89599b13fe52e528bef42286a05b9e3
OSR <osrsla@gmail.com>
2023-08-30 16:38:12 +0900

    feat: Add catVC UrlSession(optional task3-5)

--- todolist_/CatViewController.swift
+++ todolist_/CatViewController.swift
@@ -14,36 +14,68 @@ class CatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        fetchRandomCatImage()
     }
 }
 
 extension CatViewController {
     private func setup() {
         view.addSubview(imageView)
+        
         imageView.image = UIImage(systemName: "photo")
-
+        imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: 240),
-            imageView.heightAnchor.constraint(equalToConstant: 240),
+            imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+        
         ])
     }
+    
+    private func fetchRandomCatImage() {
+        let apiURL = URL(string: "https://api.thecatapi.com/v1/images/search")!
+        
+        URLSession.shared.dataTask(with: apiURL) { data, _, error in
+            if let error = error {
+                self.handleError(error, for: "fetching random cat image")
+                return
+            }
+            
+            if let data = data,
+               let catData = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]],
+               let catImageURLString = catData.first?["url"] as? String,
+               let catImageURL = URL(string: catImageURLString)
+            {
+                self.loadCatImage(from: catImageURL)
+            } else {
+                self.handleInvalidDataResponse(for: "fetching random cat image")
+            }
+        }.resume()
+    }
+    
+    private func loadCatImage(from url: URL) {
+        imageView.getImageFromURL(url: url.absoluteString) { result in
+            switch result {
+            case .success(let data):
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage(data: data)
+                }
+            case .failure(let error):
+                self.handleError(error, for: "loading cat image")
+            }
+        }
+    }
+    
+    private func handleError(_ error: Error, for action: String) {
+        print("Error \(action): \(error)")
+        // Handle the error appropriately (show an alert, etc.)
+    }
+    
+    private func handleInvalidDataResponse(for action: String) {
+        print("Error \(action): Invalid data response")
+        // Handle the error appropriately (show an alert, etc.)
+    }
 }
-
-//// UIView.animate 메서드를 사용하여 요소의 위치를 이동합니다.
-//UIView.animate(withDuration: 0.5) {
-//    self.myView.frame.origin.x += 100 // x축으로 100만큼 이동합니다.
-//}
-//
-//// UIView.animate 메서드를 사용하여 요소의 크기를 조절합니다.
-//UIView.animate(withDuration: 0.5) {
-//    self.myView.frame.size = CGSize(width: 200, height: 200) // 요소의 크기를 200x200으로 조절합니다.
-//}
-//
-//// UIView.animate 메서드를 사용하여 요소의 투명도를 조절합니다.
-//UIView.animate(withDuration: 0.5) {
-//    self.myView.alpha = 0.5 // 요소의 투명도를 0.5로 조절합니다.
-//}
--- todolist_/UIImageView + Extention.swift
+++ todolist_/UIImageView + Extention.swift
@@ -4,20 +4,7 @@
 //
 //  Created by SR on 2023/08/29.
 //
-
-import Foundation
 import UIKit
+import Foundation
 
-extension UIImageView {
-    func loadImage(from url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url),
-               let image = UIImage(data: data)
-            {
-                DispatchQueue.main.async {
-                    self?.image = image
-                }
-            }
-        }
-    }
-}
+
