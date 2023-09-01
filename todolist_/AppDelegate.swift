d0ba618b70ad03ed40d8e1f5ac615fdacdd69a61
OSR <osrsla@gmail.com>
2023-09-01 12:31:34 +0900

    final

--- todolist_/AppDelegate.swift
+++ todolist_/AppDelegate.swift
@@ -7,13 +7,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     let homeVC = HomeViewController()
 
-    let taskVC = TaskViewController()
-    let detailVC = DetailViewController()
-    let editVC = EditViewController()
-    let createVC = CreateViewController()
-
-    let catVC = CatViewController()
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
