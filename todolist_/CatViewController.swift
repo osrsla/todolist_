f130bda1325675d9d5ae708adbb5658211e96660
OSR <osrsla@gmail.com>
2023-09-03 16:44:29 +0900

    feat: Add catView bell animation

--- todolist_/CatViewController.swift
+++ todolist_/CatViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CatViewController: UIViewController {
     var imageView = UIImageView()
+    var bellButton = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,18 +24,37 @@ extension CatViewController {
         navigationItem.title = "🐈🐈‍⬛ Cat 🐈‍⬛🐈"
         
         view.addSubview(imageView)
-        
+        view.addSubview(bellButton)
+
         imageView.image = UIImage(systemName: "photo")
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        bellButton.setImage(UIImage(systemName: "bell.fill"), for: .normal)
+        bellButton.tintColor = .systemYellow
+        bellButton.addTarget(self, action: #selector(bellButtonTapped), for: .touchUpInside)
+        bellButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
-            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
-        
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
+            
+            bellButton.topAnchor.constraint(equalToSystemSpacingBelow: imageView.bottomAnchor, multiplier: 2),
+            bellButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),      
         ])
+    }
+    
+    @objc private func bellButtonTapped() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+
+        animation.isAdditive = true
+        bellButton.layer.add(animation, forKey: "shake")
     }
     
     private func fetchRandomCatImage() {
